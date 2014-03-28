5f3969e06d5bcec9e5ca6ce03c7a88ba043eae9f
Ale Sandro <alexrah@gmail.com>
2014-03-28 03:15:10 +0100

    added jqueryUI effects - CSS styles

---
+++ beautify.js
@@ -0,0 +1,14 @@
+
+setTimeout(function(){
+        $('tr').each(function(index){
+          console.log(index);
+          $(this).delay(100 * index).show(100);
+        });
+        $('#title').animate({
+          color: "#cc0066",
+          "font-size" : "16px",
+        }, 3000);
+        $('span').animate({
+          color : "#00cc66",
+        }, 3000);
+}, 500);
