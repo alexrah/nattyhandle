a55791a549de3819928ff64fdbaa0365704d42cf
Ale Sandro <alexrah@gmail.com>
2014-03-30 05:28:08 +0200

    external Helper registered from stripes.js

--- stripes.js
+++ stripes.js
@@ -1,15 +1,15 @@
   // Handlebars.registerHelper('stripeRows', function (rows, fn) {
-  function stripeRows (rows, fn){
+  function stripes (rows, options){
         var buffer = [],
             i, len;
         
         for (i = 0, len = rows.length; i < len; ++i) {
             var row = rows[i];
-            row.rowClass = (i + 1) % 2 === 0 ? 'even' : 'odd';
+            row.stripeClass = (i + 1) % 2 === 0 ? 'even' : 'odd';
 
             // Render the block once for each row.
             // buffer.push(fn(row));
-            buffer += fn(item);
+            buffer += options.fn(row);
         }
         
         return buffer;
